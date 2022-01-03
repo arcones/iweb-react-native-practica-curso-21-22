c53acf1f62066cf13bc4b4f816b3a6d8661de9a2
Marta Arcones <marta.rodrigueza@alumnos.upm.es>
2022-01-03 13:51:33 +0100

    It appears

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import GameScreen from './app/components/tictactoe/GameScreen';
+import Main from './app/components/Main';
 
 export default function App() {
   return (
-    <GameScreen/>
+    <Main/>
   );
 }
 
