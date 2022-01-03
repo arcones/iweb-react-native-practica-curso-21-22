52f8056bfd89e5476ef0a7bec3390672cfe93522
Marta Arcones <marta.rodrigueza@alumnos.upm.es>
2022-01-03 12:27:13 +0100

    Add mid folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import GameScreen from './app/components/GameScreen';
+import GameScreen from './app/components/tictactoe/GameScreen';
 
 export default function App() {
   return (
