daf21ff2bb012c0dfb3deae85e08783a7adc5949
Marta Arcones Rodríguez <marta.arcones@gmail.com>
2022-01-04 17:09:56 +0100

    Not working, promise error

--- components/NoQuizzes.js
+++ components/NoQuizzes.js
@@ -1,11 +1,11 @@
 import travolta from './img/travolta.gif'
-import { Text, Stack } from "@react-native-material/core";
+import { Text, Stack, Snackbar } from "@react-native-material/core";
 import { Image } from 'react-native';
 
 const NoQuizzes = () => {
     return (
         <Stack style={{ margin: 56 }} fill center spacing={4}>
-            <Text variant="h5">Upps... no hay preguntas disponibles</Text>
+            <Snackbar message="Intentando cargar más preguntas..." style={{ position: "absolute", start: 16, end: 16, bottom: 16 }}/>
             <Image style={{ width: 250, height: 250 }} source={travolta} alt="Travolta" />
         </Stack>
     )
