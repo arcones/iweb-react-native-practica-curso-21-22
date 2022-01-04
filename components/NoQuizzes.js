8796dd5d75df609475aca39d17b5280a6be77950
Marta Arcones Rodríguez <marta.arcones@gmail.com>
2022-01-04 17:13:26 +0100

    Move toast to top

--- components/NoQuizzes.js
+++ components/NoQuizzes.js
@@ -1,11 +1,11 @@
 import travolta from './img/travolta.gif'
-import { Text, Stack, Snackbar } from "@react-native-material/core";
+import { Stack, Snackbar } from "@react-native-material/core";
 import { Image } from 'react-native';
 
 const NoQuizzes = () => {
     return (
         <Stack style={{ margin: 56 }} fill center spacing={4}>
-            <Snackbar message="Intentando cargar más preguntas..." style={{ position: "absolute", start: 16, end: 16, bottom: 16 }}/>
+            <Snackbar message="Intentando cargar más preguntas..." style={{ position: "absolute", start: 16, end: 16, top: 16 }}/>
             <Image style={{ width: 250, height: 250 }} source={travolta} alt="Travolta" />
         </Stack>
     )
