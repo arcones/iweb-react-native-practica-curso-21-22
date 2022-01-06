8e3203bbce915c6f3e1140330bfadda426b48309
Marta Arcones Rodríguez <marta.arcones@gmail.com>
2022-01-06 23:01:21 +0100

    WIP

---
+++ components/tictactoe/Square.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {TouchableHighlight, Text, StyleSheet} from 'react-native';
+
+export default function Square(props) {
+  
+  function squareClick() {
+    if(props.value === "-") {
+      props.boardClick(props.rowIndex, props.columnIndex);
+    }
+  }
+
+  return(
+    <TouchableHighlight style={styles.square} onPress={squareClick} disabled={props.value != '-'}>
+      <Text style={styles.squareText}>
+        {props.value}
+      </Text> 
+    </TouchableHighlight>
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  square: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#eee'
+  },
+  squareText: {
+    fontSize: 50,
+  }
+
+});
